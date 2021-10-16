d7ddba4d6902808aa37c2bdee8490ada77d6cb61
Val <nikitinrus404@gmail.com>
2021-10-16 12:51:42 +0300

    HW2: test and ans.txt

---
+++ HW2/Test/Test/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class1
+{
+	internal class A
+	{
+		public void mA() {
+			Console.WriteLine("A");
+		}
+	}
+	public class B
+	{
+		public void mB()
+		{
+			Console.WriteLine("B");
+		}
+	}
+}
---
+++ HW2/Test/Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Test
+{
+	private class A
+	{
+		protected int a;
+		public int b;
+		void methodA(A a1, B b1)
+		{
+			a1.a = 1;
+			b1.a = 1;
+		}
+	}
+	public class B: A
+	{
+		void methodB(A a1, B b1)
+		{
+			a1.a = 1;
+			this.a = 10;
+			b1.a = 2;
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			A a = new A();
+			a.mA();
+		}
+	}
+}
